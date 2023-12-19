24eb0fe0edae0c08b7d35e211cb58b3586ad8adf
LevelLawrence-FS <lhlawrence@student.fullsail.edu>
2023-12-19 17:44:19 -0500

    homepage styling 90% complete

--- app/page.js
+++ app/page.js
@@ -1,7 +1,50 @@
-export default function Home() {
+import Image from "next/image";
+import reefImage from "../public/images/CoralReef_test.png";
+
+const Home = () => {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="h-screen w-full">
+      <section
+        id="intro-section"
+        className=" pt-20 z-0 relative h-full overflow-clip"
+      >
+        <h2 className="text-xl font-semibold text-white">Welcome to</h2>
+        <h1 className="text-2xl font-semibold text-orange-500">
+          Contextual Graphics.LLC
+        </h1>
+        <p className="text-white font-light pt-4 leading-snug">
+          Whether you're a startup looking to make your mark, an established
+          brand seeking a fresh look, or an individual with a unique vision, our
+          team of dedicated designers is here to transform your ideas into
+          reality. From innovative logos to captivating marketing materials, we
+          provide a full spectrum of graphic design services tailored to your
+          specific needs.
+        </p>
+        <div className="flex justify-between">
+          <div>
+            <button className="border underline underline-offset-4 mt-8 text-white py-2 px-4 rounded-lg">
+              Sign Up/ Login
+            </button>
+            <p className="text-white underline text-center pt-2 font-light text-sm">
+              Privacy Policy
+            </p>
+          </div>
+          <div>
+            <button className="bg-orange-500 mt-8 py-2 px-4 rounded-lg">
+              Contact Us
+            </button>
+            <p className="text-white underline text-center pt-2 font-light text-sm">
+              Terms of Service
+            </p>
+          </div>
+        </div>
+        <Image
+          src={reefImage}
+          className="z-10 absolute bottom-0 right-0 translate-x-14 translate-y-20"
+        />
+      </section>
     </main>
   );
-}
+};
+
+export default Home;
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,14 @@
+import { IoMenu } from "react-icons/io5";
+import Image from "next/image";
+import cgLogo from "../public/CG_LogoAndText.svg";
 const NavBar = () => {
   return (
-    <nav>
-      <h1>logo</h1>
-      <h3>Menu</h3>
-      <p>thersing</p>
+    <nav
+      className="flex justify-between items-center py-2
+     text-white"
+    >
+      <Image src={cgLogo} className="w-14 filter contrast-150 invert " />
+      <IoMenu className="text-5xl" />
     </nav>
   );
 };
